a0cb85d09a4a914dd68a5a399936d79de143e53e
Mahaveer Jangir <mahaveer1707@gmail.com>
2025-12-12 17:02:02 +0530

    making some more changes

--- country-api/app/data.py
+++ country-api/app/data.py
@@ -19,5 +19,19 @@ COUNTRIES = {
         "region": "Europe",
         "currency": "GBP",
         "population": 67886011
+    },
+    "SA": {
+        "name": "South Africa",
+        "capital": "Johnesburg",
+        "region": "Africa",
+        "currency": "RAS",
+        "population": 30019023
+    },
+    "MY": {
+        "name": "Malaysia",
+        "capital": "Kuala Lumpur",
+        "region": "Asia",
+        "currency": "MYR",
+        "population": 210994011
     }
 }
